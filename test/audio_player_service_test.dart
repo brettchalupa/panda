b4572861e9b7233310fc6f0e11d7a744e42e4d80
Brett Chalupa <brett@brettchalupa.com>
2025-11-09 16:16:35 -0500

    add audio player service test

---
+++ test/audio_player_service_test.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:stingray/audio_player_service.dart';
+import 'package:stingray/jellyfin_api.dart';
+
+void main() {
+  // Note: AudioPlayerService testing requires mocking audioplayers and audio_service
+  // which is complex. These tests focus on the data classes.
+
+  group('QueueItem', () {
+    test('can be created with required fields', () {
+      final album = Album(id: 'album1', name: 'Test Album');
+      final track = Track(id: 'track1', name: 'Track 1');
+
+      final item = QueueItem(
+        track: track,
+        album: album,
+        streamUrl: 'http://test/stream',
+        albumArtUrl: 'http://test/art',
+      );
+
+      expect(item.track, track);
+      expect(item.album, album);
+      expect(item.streamUrl, 'http://test/stream');
+      expect(item.albumArtUrl, 'http://test/art');
+    });
+
+    test('albumArtUrl can be null', () {
+      final album = Album(id: 'album1', name: 'Test Album');
+      final track = Track(id: 'track1', name: 'Track 1');
+
+      final item = QueueItem(
+        track: track,
+        album: album,
+        streamUrl: 'http://test/stream',
+        albumArtUrl: null,
+      );
+
+      expect(item.albumArtUrl, isNull);
+    });
+  });
+}
